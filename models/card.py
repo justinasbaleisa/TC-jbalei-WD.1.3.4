1add1871dff6db23c9d1171c1dad5cbc8caa965c
justinasbaleisa <justinas.baleisa@gmail.com>
2025-03-23 03:37:45 +0200

    issue 2: create Card class

--- models/card.py
+++ models/card.py
@@ -18,16 +18,42 @@ class Card:
     SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
     VALUES = {rank: index + 2 for index, rank in enumerate(RANKS)}
 
-    def __init__(self, rank, suit):
+    def __init__(self, rank: str, suit: str):
         self.rank = rank
         self.suit = suit
-        self.value = Card.VALUES[rank]
+        self.value = Card.VALUES[self.rank]
 
     def __str__(self):
         return f"Card: {self.rank} of {self.suit} valued '{self.value}'"
 
-    def __eq__(self, other):
+    def __eq__(self, other: int) -> bool:
         return self.value == other.value
 
-    def __gt__(self, other):
+    def __gt__(self, other: int) -> bool:
         return self.value > other.value
+
+    @property
+    def rank(self):
+        return self._rank
+
+    @rank.setter
+    def rank(self, value: str):
+        if isinstance(value, str):
+            value = value.capitalize()
+            if value in Card.RANKS:
+                self._rank = value
+                return
+        raise ValueError(f"Invalid rank '{value}' of type '{type(value).__name__}'")
+
+    @property
+    def suit(self):
+        return self._suit
+
+    @suit.setter
+    def suit(self, value: str) -> None:
+        if isinstance(value, str):
+            value = value.capitalize()
+            if value in Card.SUITS:
+                self._suit = value
+                return
+        raise ValueError(f"Invalid suit '{value}' of type '{type(value).__name__}'")
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,3 +1,51 @@
 import pytest
 
-pass
+from models.card import Card
+
+
+@pytest.mark.parametrize(
+    "rank, suit",
+    [
+        (rank, suit)
+        for rank in Card.RANKS
+        + [rank.lower() for rank in Card.RANKS]
+        + [rank.upper() for rank in Card.RANKS]
+        for suit in Card.SUITS
+    ],
+)
+def test_card_valid(rank, suit):
+    card = Card(rank, suit)
+    assert card.rank == rank.capitalize()
+    assert card.suit == suit.capitalize()
+    assert card.value == Card.VALUES[rank.capitalize()]
+
+
+@pytest.mark.parametrize("rank", ["1", "11", "", 1, "karalius", None])
+def test_card_invalid_rank(rank):
+    with pytest.raises(ValueError) as e:
+        Card(rank, "Hearts")
+    assert "Invalid rank" in str(e.value)
+
+
+@pytest.mark.parametrize("suit", [1, "", "bugnai", None])
+def test_card_invalid_suit(suit):
+    with pytest.raises(ValueError) as e:
+        Card("2", suit)
+    assert "Invalid suit" in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "rank, suit, value",
+    [
+        ("2", "Hearts", 2),
+        ("6", "Diamonds", 6),
+        ("10", "Clubs", 10),
+        ("Jack", "Spades", 11),
+        ("Queen", "Hearts", 12),
+        ("King", "Diamonds", 13),
+        ("Ace", "Clubs", 14),
+    ],
+)
+def test_card_valid_value(rank, suit, value):
+    card = Card(rank, suit)
+    assert card.value == value
